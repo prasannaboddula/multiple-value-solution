2e53e16aa8641f238e8dc7db6d0e717b555c3130
prasannaboddula <prasannaboddula749@gmail.com>
2022-06-24 13:40:46 +0000

    multiple value solution

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,10 @@
+const myArray = ["camel", 256, true, "5.6"];
+const bulb = {
+  watts: 10,
+  type: "LED",
+};
+const multiplyByFour = (num) => {
+  return num * 4;
+};
+
+export { myArray, bulb, multiplyByFour };
